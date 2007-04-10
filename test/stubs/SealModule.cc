92bc713779fefcde85afe23d9bbfc96025384229
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2007-04-10 23:07:39 +0000

    Adapt to new plug-in manager

--- test/stubs/SealModule.cc
+++ test/stubs/SealModule.cc
@@ -1,5 +1,5 @@
 #include "FWCore/Framework/interface/MakerMacros.h"
-#include "PluginManager/ModuleDef.h"
+#include "FWCore/PluginManager/interface/ModuleDef.h"
 DEFINE_SEAL_MODULE();
 
 #include "DQM/SiStripCommon/test/stubs/test_MessageLogger.h"
