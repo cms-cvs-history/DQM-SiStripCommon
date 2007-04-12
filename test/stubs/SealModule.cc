9374d9b834e66a7b5e6ead8f6bf9c8e956fafdf3
cmsbuild <>
2007-04-12 17:04:45 +0000

    This commit was manufactured by cvs2git to create tag 'DQM-SiStripCommon- CMSSW_1_5_0_pre1'.
    Sprout from master 2007-04-12 17:04:44 UTC William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch> 'Use new plug-in manager'
    Delete:
        test/stubs/SealModule.cc
        test/stubs/test_MessageLogger.cc
        test/stubs/test_MessageLogger.h

--- test/stubs/SealModule.cc
+++
@@ -1,6 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-#include "FWCore/PluginManager/interface/ModuleDef.h"
-DEFINE_SEAL_MODULE();
-
-#include "DQM/SiStripCommon/test/stubs/test_MessageLogger.h"
-DEFINE_ANOTHER_FWK_MODULE(test_MessageLogger);
