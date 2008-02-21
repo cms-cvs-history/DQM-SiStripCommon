2c1562b74fce24d5627ff74b104949d696b8fc6b
Robert John Bainbridge <sha1-23e31ff3811f548144cca79b92e3ea0df433ced9@cern.ch>
2008-02-21 14:03:24 +0000

    tidy up of package

--- test/plugins/module.cc
+++
@@ -1,8 +0,0 @@
-#include "FWCore/Framework/interface/MakerMacros.h"
-DEFINE_SEAL_MODULE();
-
-#include "DQM/SiStripCommon/test/plugins/test_MessageLogger.h"
-DEFINE_ANOTHER_FWK_MODULE(test_MessageLogger);
-
-#include "DQM/SiStripCommon/test/plugins/test_ParameterSet.h"
-DEFINE_ANOTHER_FWK_MODULE(test_ParameterSet);
